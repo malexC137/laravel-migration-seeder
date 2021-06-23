a4f40e5da08bb1bac7ee88e1e5daf56cfc901076
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-06-24 00:05:31 +0200

    Ex yet to be finished

---
+++ laravel-migration-seeder/database/migrations/2021_06_23_204410_update_fields_to_holidays_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateFieldsToHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('holidays', function (Blueprint $table) {
+            $table->char('name', 100)->comment('Package name');
+            $table->string('description', 255)->nullable();
+            $table->char('modality', 100)->comment('All inclusive etc');
+            $table->integer('price');
+            $table->string('country');
+            $table->smallInteger('cod_manager');
+            $table->integer('length')->after('name ');
+            $table->char('transport', 100)->comment('Airplane, boat, train, bus')->after('modality')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('holidays', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('description');
+            $table->dropColumn('modality');
+            $table->dropColumn('price');
+            $table->dropColumn('country');
+            $table->dropColumn('cod_manager');
+            $table->dropColumn('length');
+            $table->dropColumn('transport');
+        });
+    }
+}
